4e29ee406d624642d38cdf5c527c10708dc1a4e3
UmairSubhani <umair.subhani@acetsolutions.com>
2022-02-14 11:20:18 +0500

    Breadcums in to_heading

--- resources/views/layouts/top_heading.blade.php
+++ resources/views/layouts/top_heading.blade.php
@@ -1,8 +1,29 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-            <h4 class="mb-sm-0 font-size-18 top_heading_text">{{ $heading ?? '' }}</h4>
+            <h5 class="mb-sm-0 font-size-18 top_heading_text">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-flex align-items-center justify-content-between">
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="{{url('/')}}">Dashboard ></a></li>
+                                    <?php $link = "" ?>
+                                    @for($i = 1; $i <= count(Request::segments()); $i++)
+                                        @if($i < count(Request::segments()) & $i > 0)
+                                            <?php $link .= "/" . Request::segment($i); ?>
+                                            <a href="<?= $link ?>">{{ ucwords(str_replace('_',' ',Request::segment($i)))}}</a> >
+                                        @else {{ucwords(str_replace('_',' ',Request::segment($i)))}}
+                                        @endif
+                                    @endfor
 
+                                </ol>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </h5>
             @if(isset($goBack))
                 <button onclick="location.href='{{ $goBack }}'" type="button" class="mt-4 btn waves-effect">
                     <i style="font-size: 15px" class="fas fa-backspace"> Go Back</i>
