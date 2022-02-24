e6f00535c231697d69bfd4a25fc1c0c6a3b87cfa
hassansg1 <hassanmumtaz0@gmail.com>
2022-02-24 13:22:38 +0500

    new changes

--- resources/views/tree_files/clause_table.blade.php
+++ resources/views/tree_files/clause_table.blade.php
@@ -5,7 +5,7 @@
 {{--    </td>--}}
     <td style="width:50px;padding-left: {{ $padding }}px">{{ $item->number }}</td>
     <td>{{ $item->title }}</td>
-    <td>{{ substr($item->description,0,20) }}</td>
+    <td>{{ $item->description }}</td>
 {{--    <td>--}}
 {{--        @include('components.edit_delete_button')--}}
 {{--    </td>--}}
