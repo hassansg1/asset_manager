08fc6e4e6422d03877d53f80806461cd172fb910
UmairSubhani <umair.subhani@acetsolutions.com>
2022-02-24 13:19:22 +0500

    STANDARD-CLAUSES-SESSION-ID-CHANGINGS

--- resources/views/clause/form.blade.php
+++ resources/views/clause/form.blade.php
@@ -8,24 +8,29 @@
             <div class="card-body">
                 <h4 class="card-title mb-4">{{ $heading }} Information</h4>
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
                         <div class="mb-3">
-                            <label for="{{ isset($item) ? $item->id:'' }}standard_id"
-                                   class="form-label">Standard</label>
-                            <input type="hidden" name="standard_id"
-                                   value="{{ \Illuminate\Support\Facades\Session::get('standard_id') ?? '' }}">
-                            <select class="form-select form-select-input" name="standard_id"
-                                    id="{{ isset($item) ? $item->id:'' }}standard_id">
-                                @foreach(\App\Models\Standard::all() as $value)
-                                    <option value=""></option>
-                                    <option
-                                        {{ $value->id == (isset($item) ? $item->standard_id:old('last_name') ?? (\Illuminate\Support\Facades\Session::get('standard_id') ?? '')) ? 'selected' : ''  }}
-                                        value="{{ $value->id }}">{{ $value->name }}</option>
-                                @endforeach
-                            </select>
+                            <label for="{{ isset($item) ? $item->id:'' }}title" class="form-label">Title</label>
+                            <input type="text" value="{{ isset($item) ? $item->title:old('title') ?? ''  }}"
+                                   class="form-control" id="{{ isset($item) ? $item->id:'' }}title" name="title">
+{{--                            <input type="text" class="form-control" name="name" id="name" readonly value="{{Session::get('standard')->name}}">--}}
+                            <input type="hidden" class="form-control" name="standard_id" id="standard_id" value="{{Session::get('standard_id')}}">
+{{--                            <label for="{{ isset($item) ? $item->id:'' }}standard_id"--}}
+{{--                                   class="form-label">Standard</label>--}}
+{{--                            <input type="hidden" name="standard_id"--}}
+{{--                                   value="{{ \Illuminate\Support\Facades\Session::get('standard_id') ?? '' }}">--}}
+{{--                            <select class="form-select form-select-input" name="standard_id"--}}
+{{--                                    id="{{ isset($item) ? $item->id:'' }}standard_id">--}}
+{{--                                @foreach(\App\Models\Standard::all() as $value)--}}
+{{--                                    <option value=""></option>--}}
+{{--                                    <option--}}
+{{--                                        {{ $value->id == (isset($item) ? $item->standard_id:old('last_name') ?? (\Illuminate\Support\Facades\Session::get('standard_id') ?? '')) ? 'selected' : ''  }}--}}
+{{--                                        value="{{ $value->id }}">{{ $value->name }}</option>--}}
+{{--                                @endforeach--}}
+{{--                            </select>--}}
                         </div>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
                         <div class="mb-3">
                             <label for="{{ isset($item) ? $item->id:'' }}number"
                                    class="form-label required">Number</label>
@@ -33,16 +38,16 @@
                                    class="form-control" id="{{ isset($item) ? $item->id:'' }}number" name="number">
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="mb-3">
-                            <label for="{{ isset($item) ? $item->id:'' }}title" class="form-label">Title</label>
-                            <input type="text" value="{{ isset($item) ? $item->title:old('title') ?? ''  }}"
-                                   class="form-control" id="{{ isset($item) ? $item->id:'' }}title" name="title">
-                        </div>
-                    </div>
+{{--                    <div class="col-lg-4">--}}
+{{--                        <div class="mb-3">--}}
+{{--                            <label for="{{ isset($item) ? $item->id:'' }}title" class="form-label">Title</label>--}}
+{{--                            <input type="text" value="{{ isset($item) ? $item->title:old('title') ?? ''  }}"--}}
+{{--                                   class="form-control" id="{{ isset($item) ? $item->id:'' }}title" name="title">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-12">
                         <div class="mb-3">
                             <label for="{{ isset($item) ? $item->id:'' }}description"
                                    class="form-label">Description</label>
--- resources/views/clause_edit/form.blade.php
+++ resources/views/clause_edit/form.blade.php
@@ -8,22 +8,15 @@
             <div class="card-body">
                 <h4 class="card-title mb-4">{{ $heading }} Information</h4>
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
                         <div class="mb-3">
-                            <label for="{{ isset($item) ? $item->id:'' }}standard_id"
-                                   class="form-label">Standard</label>
-                            <select class="form-select form-select-input" name="standard_id"
-                                    id="{{ isset($item) ? $item->id:'' }}standard_id">
-                                @foreach(\App\Models\Standard::all() as $value)
-                                    <option value=""></option>
-                                    <option
-                                        {{ $value->id == (isset($item) ? $item->standard_id:old('last_name') ?? '') ? 'selected' : ''  }}
-                                        value="{{ $value->id }}">{{ $value->name }}</option>
-                                @endforeach
-                            </select>
+                            <label for="{{ isset($item) ? $item->id:'' }}title" class="form-label">Title</label>
+                            <input type="text" value="{{ isset($item) ? $item->title:old('title') ?? ''  }}"
+                                   class="form-control" id="{{ isset($item) ? $item->id:'' }}title" name="title">
+                            <input type="hidden" class="form-control" name="standard_id" id="standard_id" value="{{Session::get('standard_id')}}">
                         </div>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
                         <div class="mb-3">
                             <label for="{{ isset($item) ? $item->id:'' }}number"
                                    class="form-label required">Number</label>
@@ -31,16 +24,9 @@
                                    class="form-control" id="{{ isset($item) ? $item->id:'' }}number" name="number">
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="mb-3">
-                            <label for="{{ isset($item) ? $item->id:'' }}title" class="form-label">Title</label>
-                            <input type="text" value="{{ isset($item) ? $item->title:old('title') ?? ''  }}"
-                                   class="form-control" id="{{ isset($item) ? $item->id:'' }}title" name="title">
-                        </div>
-                    </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-12">
                         <div class="mb-3">
                             <label for="{{ isset($item) ? $item->id:'' }}description"
                                    class="form-label">Description</label>
