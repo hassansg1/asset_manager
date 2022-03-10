f2a67b917110d96b86d844db42f66720b0fff5e3
UmairSubhani <umair.subhani@acetsolutions.com>
2022-03-10 17:36:43 +0500

    APPLICABLE-CLAUSE-ENABLE-DISABLE-FUNCTIONALTIY

--- resources/views/patch/form_rows.blade.php
+++ resources/views/patch/form_rows.blade.php
@@ -1,12 +1,15 @@
 @foreach($items as $item)
     <tr id="{{ $item->id }}">
-        <td colspan="1"><input
+        <td colspan="1">
+
+            <input
                 {{ !$item->is_critical ? 'disabled' : '' }}
                 data-type="{{ $route ?? '' }}"
                 data-software="{{ $item->software->id ?? null }}"
                 data-show_name="{{ $item->show_name ?? null }}"
                 type="checkbox" name="select_row" value="{{ $item->id }}"
-                id="select_check_{{ $item->id }}" class="select_row select_patch_cb"></td>
+                id="select_check_{{ $item->id }}" class="select_row select_patch_cb">
+        </td>
         <td>{{ $item->name ?? '' }}</td>
         <td>{{ $item->software->name ?? '' }}</td>
         <td>{{ $item->software->version ?? '' }}</td>
