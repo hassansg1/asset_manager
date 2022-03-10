3bf1296baca095ef6f7effbaee97e506ac571d9b
UmairSubhani <umair.subhani@acetsolutions.com>
2022-03-10 17:50:52 +0500

    APPLICABLE-CLAUSE-ENABLE-DISABLE-FUNCTIONALTIY

--- resources/views/patch/form_rows.blade.php
+++ resources/views/patch/form_rows.blade.php
@@ -6,7 +6,9 @@
                 {{ !$item->is_critical ? 'disabled' : '' }}
                 data-type="{{ $route ?? '' }}"
                 data-software="{{ $item->software->id ?? null }}"
+               @if($item->software)
                 data-show_name="{{ $item->show_name ?? null }}"
+                @endif
                 type="checkbox" name="select_row" value="{{ $item->id }}"
                 id="select_check_{{ $item->id }}" class="select_row select_patch_cb">
         </td>
