615fdb21c3bdb54aceefe71d0a2d01c6c8ed99cf
UmairSubhani <umair.subhani@acetsolutions.com>
2022-03-30 17:36:56 +0500

    MINOR-CHANGINGS

---
+++ app/Repos/SystemUserRepo.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repos;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class SystemUserRepo
+{
+    //
+    public $query;
+
+    /**
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function filter($query, $request)
+    {
+        $this->query = $query;
+
+        $this->applyFilterOtcmUser($request);
+
+        return $this->query;
+    }
+
+    public function applyFilterOtcmUser($request)
+    {
+        $this->query = $this->query->where('user_type', 'SYSTEM-USER');
+    }
+}
