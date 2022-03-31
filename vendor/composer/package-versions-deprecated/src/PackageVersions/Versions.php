bb7240c52237e897b64b40181ece65757de9a061
hassansg1 <hassanmumtaz0@gmail.com>
2022-03-31 09:58:12 +0500

    ;

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -165,7 +165,7 @@ final class Versions
   'sebastian/type' => '2.3.4@b8cd8a1c753c90bc1a0f5372170e3e489136f914',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.1@34a41e998c2183e22995f158c581e7b5e755ab9e',
-  'laravel/laravel' => 'dev-master@f6bb47cd7e5e48293d0be22763b8cb6ed7ba4c1b',
+  'laravel/laravel' => 'dev-master@a573c2f2bb374304cbc088c19f345fc4910c29eb',
 );
 
     private function __construct()
