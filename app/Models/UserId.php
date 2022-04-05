6039627d19351120c19914c1da0d8d2f7ca963e2
UmairSubhani <umair.subhani@acetsolutions.com>
2022-04-05 15:54:20 +0500

    MINOR-CHANGINGS

--- app/Models/UserId.php
+++ app/Models/UserId.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserId extends Model
 {
-	use HasFactory;
+    use HasFactory;
     protected $fillable = [
         'user_id',
         'parent_id',
@@ -20,7 +20,7 @@ class UserId extends Model
         return [
             'right_id'=>'required',
             'condition' => 'required',
-            'user_id' => 'required|unique:user_ids,user_id,NULL,id,parent_id,' . $parentId,
+            'user_id' => 'required|unique:user_ids,user_id,NULL,id,parent_id,'.$parentId,
         ];
     }
     public function user_rights_id(){
@@ -35,72 +35,46 @@ class UserId extends Model
     public function user_id_systems(){
         return $this->belongsTo(System::class, 'parent_id', 'id');
     }
-	public function saveFormData($item, $request)
-	{
+    public function saveFormData($item, $request)
+    {
         $res=UserRight::where('parent_id',$request->id)->delete();
-        if ($request->user_type == "asset") {
-		if (isset($request->user_id)) $item->user_id = $request->user_id;
-            if (isset($request->asset_id)) $item->parent_id = $request->asset_id;
-            if (isset($request->condition)) $item->condition = $request->condition;
-            if (isset($request->approvel_expirey_date)) $item->approvel_expirey_date = $request->approvel_expirey_date;
-        $item->parent = "asset";
-		if (isset($request->description)) $item->description = $request->description;
-		$item->save();
-            if($request->right_id){
-                $rights= $request->right_id;
-                if($item && $rights){
-                    foreach ($rights as $right){
-                        $assets_rights = UserRight::updateOrCreate(
-                            [
-                                'right_id'     => $right,
-                                'parent_id'   => $item->id,
-                            ],
-                            [
-                                'parent_type' => "asset"
-                            ]
-                        );
-                    }
-                }
-            }
-		}
-
-	if($request->user_type == "system"){
         if (isset($request->user_id)) $item->user_id = $request->user_id;
-        if (isset($request->system_id)) $item->parent_id = $request->system_id;
-        $item->parent = "system";
+        if ($request->user_type == "asset") {
+            if (isset($request->asset_id)) $item->parent_id = $request->asset_id;
+            $item->parent = $request->user_type;
+        }else{
+            if (isset($request->system_id)) $item->parent_id = $request->system_id;
+            $item->parent = $request->user_type;
+
+        }
+        if (isset($request->condition)) $item->condition = $request->condition;
+        if (isset($request->approvel_expirey_date)) $item->approvel_expirey_date = $request->approvel_expirey_date;
         if (isset($request->description)) $item->description = $request->description;
         $item->save();
 
-        if($request->right_id){
-            $rights= $request->right_id;
-            if($item && $rights){
-                foreach ($rights as $right) {
-                    $assets_rights = UserRight::updateOrCreate(
-                        [
-                            'right_id'     => $right,
-                            'parent_id'   => $item->id,
-                        ],
-                        [
-                            'parent_type' => "system"
-                        ]
-                    );
-                }
+        $rights= $request->right_id;
+        if($item && $rights){
+            foreach ($rights as $right){
+                $assets_rights = new UserRight();
+                $assets_rights->parent_id = $item->id;
+                $assets_rights->right_id = $right;
+                $assets_rights->parent_type = $item->parent;
+                $assets_rights->save();
             }
         }
-	}
 
         if($request->otcm_user_id){
             $otcm_user_id= $request->otcm_user_id;
             if($item && $otcm_user_id){
 //                foreach ($otcm_user_id as $key=>$value)  {
-                    $assets_rights = new UserAccount();
-                    $assets_rights->account_id = $item->id;
-                    $assets_rights->user_id   = $otcm_user_id;
-                    $assets_rights->save();
+                $assets_rights = new UserAccount();
+                $assets_rights->account_id = $item->id;
+                $assets_rights->user_id   = $otcm_user_id;
+                $assets_rights->save();
 //                }
             }
         }
 
-	return $item;
-}
+        return $item;
+    }
 }
