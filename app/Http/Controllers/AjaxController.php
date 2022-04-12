eebc9719d4e67227a288f27d662d0ddf4338df90
UmairSubhani <umair.subhani@acetsolutions.com>
2022-04-12 12:27:27 +0500

    PORT-ISSUE-NETWORK-ASSETS

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\DB;
 class AjaxController extends Controller
 {
     //
+
     public function getNewAjaxRow(Request $request)
     {
         return response()->json(
