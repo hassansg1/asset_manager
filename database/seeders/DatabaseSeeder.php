ff71a8b9ba9251eae8090c5958836f53ae4efd59
UmairSubhani <umair.subhani@acetsolutions.com>
2022-05-13 12:07:08 +0500

    Reports tab comented

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,7 +32,7 @@ class DatabaseSeeder extends Seeder
                 "password" => Hash::make("123456789"),
             ]);
         $user = User::find(1);
-        $role = Role::updateOrCreate(["name" => "Super Admin"], ["name" => "Super Admin"]);
+         Role::updateOrCreate(["name" => "Super Admin"], ["name" => "Super Admin"]);
         $user->assignRole("Super Admin");
 //        $crud = ['company', 'unit', 'site', 'subsite', 'building', 'room', 'cabinet', 'asset','user', 'networks_list', 'systems', 'rights', 'user_ids', 'vendors', 'installed_softwares', 'softwares', 'software_components', 'installed_patches', 'patches', 'firewall_managments', 'risks', 'risk_assessments'];
 //
@@ -46,7 +46,7 @@ class DatabaseSeeder extends Seeder
 //            Permission::updateOrCreate(["name" => 'Export ' . $cr], ["name" => 'Export ' . $cr, 'guard_name' => 'web', 'group' => $cr]);
 //        }
 
-        $role->syncPermissions(getAllPermissions());
+//        $role->syncPermissions(getAllPermissions());
 
 
         // \App\Models\User::factory(10)->create();
