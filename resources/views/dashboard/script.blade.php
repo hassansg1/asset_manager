3df0cefd5e58012c6e672dd842e76475ddd60545
UmairSubhani <umair.subhani@acetsolutions.com>
2022-05-18 10:48:15 +0500

    Drop on assets and role permission working

--- resources/views/dashboard/script.blade.php
+++ resources/views/dashboard/script.blade.php
@@ -1,5 +1,7 @@
 <script>
     renderComplianceChart();
+    assetsChart();
+    assetsFunctions();
     function renderComplianceChart(parentClauseId = null) {
         let versionId = $('#select_version').val();
         let locationId = $('#select_location').val();
@@ -48,6 +50,108 @@
                         ]
                     };
                     option && myChart.setOption(option);
+                }
+            },
+        });
+    }
+
+    function  assetsChart(){
+        $.ajax({
+            type: "GET",
+            url: '{{ url('chart/assets_chart') }}',
+            success: function (result) {
+                if (result.status) {
+                    var chartDom = document.getElementById('assetsChart');
+                    var myChart = echarts.init(chartDom);
+                    var option;
+
+                    option = {
+                        title: {
+                            text: 'Number of Assets',
+                            subtext: '',
+                            left: 'center'
+                        },
+                        tooltip: {
+                            trigger: 'item'
+                        },
+                        legend: {
+                            orient: 'vertical',
+                            left: 'left'
+                        },
+                        series: [
+                            {
+                                name: 'Access From',
+                                type: 'pie',
+                                radius: '50%',
+                                data: result.data,
+                                emphasis: {
+                                    itemStyle: {
+                                        shadowBlur: 10,
+                                        shadowOffsetX: 0,
+                                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                    }
+                                }
+                            }
+                        ]
+                    };
+
+                    option && myChart.setOption(option);
+                }
+            },
+        });
+    }
+
+    function assetsFunctions(){
+        $.ajax({
+            type: "GET",
+            url: '{{ url('chart/assets_functions_chart') }}',
+            success: function (result) {
+                if (result.status) {
+                    var chartDom = document.getElementById('assetsFunctions');
+                    var myChart = echarts.init(chartDom);
+                    var option;
+                    option = {
+                        title: {
+                            text: '',
+                            left: 'center'
+                        },
+                        tooltip: {
+                            trigger: 'item'
+                        },
+
+                        series: [
+                            {
+                                name: result.data.name,
+                                type: 'pie',
+                                radius: '100%',
+                                center: ['50%', '50%'],
+                                data: result.data,
+                                roseType: 'circle',
+                                label: {
+                                    color: 'rgba(0, 0, 0)'
+                                },
+                                labelLine: {
+                                    lineStyle: {
+                                        color: 'rgba(0, 0, 0)'
+                                    },
+                                    smooth: 0.2,
+                                    length: 10,
+                                    length2: 20
+                                },
+                                itemStyle: {
+                                    color: '#b23431',
+                                },
+                                animationType: 'scale',
+                                animationEasing: 'elasticOut',
+                                animationDelay: function (idx) {
+                                    return Math.random() * 200;
+                                }
+                            }
+                        ]
+                    };
+
+                    option && myChart.setOption(option);
+
                 }
             },
         });
