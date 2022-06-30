9027a4f0623317633285fc1549f47b2731d2127f
hassansg1 <hassanmumtaz0@gmail.com>
2022-06-30 15:26:58 +0500

    ;

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -56,6 +56,7 @@ class VendorController extends BaseController
      */
     public function create()
     {
+        dd("As");
         return view($this->route . "/create")
             ->with(['route' => $this->route, 'heading' => $this->heading]);
     }
